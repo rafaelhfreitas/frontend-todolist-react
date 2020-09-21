ab122657a73844e8afa213c66e92861f46ae91d5
Rafael Freitas <rafael.freitas@sankhya.com.br>
2020-09-20 23:57:06 -0300

    First commit

---
+++ src/todo/todoList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr>
+                <td>
+                    {todo.description}
+                </td>
+            </tr>
+        ))
+    }
+
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
