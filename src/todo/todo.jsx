9ffe603f77bc05b6d5794a1258b249c563e63fb7
Rafael Freitas <rafael.freitas@sankhya.com.br>
2020-09-21 00:51:02 -0300

    Finalizando o todo app

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -14,20 +14,35 @@ export default class Todo extends Component {
     constructor(props) {
         super(props)
         this.state = { description: '', list:[] }
+
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+
 
         this.refresh()
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` :  ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({...this.state, description, list: resp.data}))
     }
 
     handleChange(e) {
         this.setState({...this.state, description: e.target.value})
     }
+
+
+    handleSearch(){
+        this.refresh(this.state.description)
+
+    }
+
 
     handleAdd() {
         const description = this.state.description
@@ -36,15 +51,44 @@ export default class Todo extends Component {
     }
 
 
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh(this.state.description))
+    }
+
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true})
+            .then(resp => this.refresh(this.state.description))
+    }
+
+
+    handleMarkAsPending(todo){
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+            .then(resp => this.refresh(this.state.description))
+
+    }
+
+    handleClear() {
+        this.refresh()
+    }
+
 
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm description={this.state.description}
+                <TodoForm 
+                    description={this.state.description}
                     handleChange={this.handleChange}
-                    handleAdd={this.handleAdd} />
-                <TodoList list={this.state.list}/>
+                    handleAdd={this.handleAdd}
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear} />
+                <TodoList 
+                    list={this.state.list}
+                    handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}/>
             </div>
         )
     }
--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
 export default props => {
 
     const renderRows = () => {
         const list = props.list || []
         return list.map(todo => (
-            <tr>
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone' : ''}> {todo.description}</td>
                 <td>
-                    {todo.description}
+                    <IconButton 
+                        style='success'
+                        icon='check'
+                        onClick={ () => props.handleMarkAsDone(todo)}
+                        hide={todo.done}/>
+                    <IconButton 
+                        style='warning'
+                        icon='undo'
+                        hide={!todo.done}
+                        onClick={() => props.handleMarkAsPending(todo)}/>
+                    <IconButton 
+                        style='danger' 
+                        icon='trash-o'
+                        hide={!todo.done}
+                        onClick={ () => props.handleRemove(todo)} />
                 </td>
             </tr>
         ))
@@ -15,10 +31,11 @@ export default props => {
 
 
     return (
-        <table className="table">
+        <table className='table'>
             <thead>
                 <tr>
                     <th>Descrição</th>
+                    <th className='taleActions'>Ações</th>
                 </tr>
             </thead>
             <tbody>
