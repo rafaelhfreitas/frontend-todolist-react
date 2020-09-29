ab465df477d2d246b7ac9d6e19e1926c7e44b91b
Rafael Freitas <rafael.freitas@sankhya.com.br>
2020-09-28 23:03:53 -0300

    Configuraçao action search

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -1,26 +1,15 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-            _id: 1,
-            description: 'Pagar fatura do cartão ',
-            done: true
-        },
-        {
-            _id: 2,
-            description: 'Reunião com a equipe',
-            done: false
-        },{
-            _id: 3,
-            description: 'Consulta médica na quarta',
-            done: false
-        }]
+    description: '',
+    list: []
 }        
 
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'DESCRIPTION_CHANGE' : 
-            return {...state, description: action_payload }
+            return {...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data}
         default: 
             return state
     }
