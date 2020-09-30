5fc702bef25c899c1611148af408469a60d7aaed
Rafael Freitas <rafael.freitas@sankhya.com.br>
2020-09-29 22:12:56 -0300

    Configurando botao de limpar o formulario

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -3,7 +3,7 @@ import {connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import IconButton from '../template/iconButton'
-import { markAsDone, markAsPending } from './todoActions'
+import { markAsDone, markAsPending , remove} from './todoActions'
 
 const TodoList = props => {
 
@@ -27,7 +27,7 @@ const TodoList = props => {
                         style='danger' 
                         icon='trash-o'
                         hide={!todo.done}
-                        onClick={ () => props.handleRemove(todo)} />
+                        onClick={ () => props.remove(todo)} />
                 </td>
             </tr>
         ))
@@ -39,7 +39,7 @@ const TodoList = props => {
             <thead>
                 <tr>
                     <th>Descrição</th>
-                    <th className='taleActions'>Ações</th>
+                    <th className='tableActions'>Ações</th>
                 </tr>
             </thead>
             <tbody>
@@ -52,5 +52,5 @@ const TodoList = props => {
 
 
 const mapStateToProps = state => ({ list: state.todo.list})
-const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
